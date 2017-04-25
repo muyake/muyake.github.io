40135586423d9fe8930122bf8b850e5679adb7fd
muyake <1245570818@qq.com>
2017-04-25 12:05:00 +0800

    first commint

---
+++ 2.js
@@ -0,0 +1,484 @@
+var canvas = document.getElementById('mycanvas'),
+	ctx = canvas.getContext('2d'),
+	w = canvas.width,
+	h = canvas.height,
+	maxCloudVelocity = 1,
+	FRAMESPEED = 16;
+var TOTALFRAME = 176;
+var TOTALTIME = FRAMESPEED * TOTALFRAME;
+
+var totalProgress = 0;
+var spriteOption = {
+	planet: {
+		width: 750,
+		height: 456,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 483
+	},
+	balloon1: {
+		width: 134,
+		height: 274,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	},
+	balloon2: {
+		width: 141,
+		height: 254,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	},
+	balloon3: {
+		width: 186,
+		height: 344,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	},
+	balloon4: {
+		width: 117,
+		height: 224,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	},
+	balloon5: {
+		width: 45,
+		height: 112,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	},
+	star: {
+		width: 81,
+		height: 86,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	},
+	cloud: {
+		width: 313,
+		height: 302,
+		imgx: 0,
+		imgy: 0,
+		x: 0,
+		y: 0
+	}
+
+};
+var starSpriteOption = [{
+	left: 558,
+	top: 569,
+	scale: 1,
+	startframe: 0,
+	endframe: 72,
+	flashCount: 6,
+}, {
+	left: 113,
+	top: 708,
+	scale: 0.4,
+	startframe: 7,
+	endframe: 79,
+}, {
+	left: 270,
+	top: 755,
+	scale: 0.6,
+	startframe: 12,
+	endframe: 84,
+}, {
+	left: 397,
+	top: 427,
+	scale: 0.4,
+	startframe: 15,
+	endframe: 87,
+}, {
+	left: 568,
+	top: 827,
+	scale: 0.4,
+	startframe: 85,
+	endframe: 157,
+}, {
+	left: 141,
+	top: 254,
+	scale: 0.8,
+	startframe: 65,
+	endframe: 197,
+	flashCount: 14,
+}, {
+	left: 642,
+	top: 427,
+	scale: 0.8,
+	flashCount: 10,
+	startframe: 35,
+	endframe: 190,
+}, {
+	left: 430,
+	top: 226,
+	scale: 0.8,
+	startframe: 75,
+	endframe: 147,
+}, {
+	left: 139,
+	top: 939,
+	scale: 0.6,
+	startframe: 48,
+	endframe: 185,
+	flashCount: 10,
+}, {
+	left: 659,
+	top: 1037,
+	scale: 0.7,
+	startframe: 55,
+	endframe: 127,
+}];
+var ImageUrlList = {
+	planet: "planet.png",
+	balloon1: "balloon1.png",
+	balloon2: "balloon2.png",
+	balloon3: "balloon3.png",
+	balloon4: "balloon4.png",
+	balloon5: "balloon5.png",
+	star: "star.png",
+	cloud: "cloud.png",
+};
+var balloonSpriteOption = [{
+	url: "balloon1.png",
+	balloon: spriteOption.balloon1,
+	top: h,
+	left: 220,
+	endLeft: 220,
+	scale: 1,
+	startframe: 0,
+	endframe: 72,
+}, {
+	url: "balloon5.png",
+	balloon: spriteOption.balloon5,
+	top: h,
+	left: -5,
+	endLeft: -25,
+	scale: 1,
+	startframe: 8,
+	endframe: 66,
+}, {
+	url: "balloon4.png",
+	balloon: spriteOption.balloon4,
+	top: h,
+	left: 326,
+	endLeft: 326,
+	scale: 1,
+	startframe: 13,
+	endframe: 178,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: 110,
+	endLeft: 110,
+	scale: 1,
+	startframe: 18,
+	endframe: 86,
+}, {
+	url: "balloon1.png",
+	balloon: spriteOption.balloon1,
+	top: h,
+	left: 308,
+	endLeft: 489,
+	scale: 1.3,
+	startframe: 27,
+	endframe: 90,
+}, {
+	url: "balloon3.png",
+	balloon: spriteOption.balloon3,
+	top: h,
+	left: 161,
+	endLeft: 278,
+	scale: 0.8,
+	startframe: 38,
+	endframe: 128,
+}, {
+	url: "balloon3.png",
+	balloon: spriteOption.balloon3,
+	top: h,
+	left: 220,
+	endLeft: 440,
+	scale: 1,
+	startframe: 38,
+	endframe: 104,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: -40,
+	endLeft: 114,
+	scale: 0.9,
+	startframe: 45,
+	endframe: 117,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: -40,
+	endLeft: 114,
+	scale: 0.9,
+	startframe: 45,
+	endframe: 117,
+}, {
+	url: "balloon4.png",
+	balloon: spriteOption.balloon4,
+	top: h,
+	left: 126,
+	endLeft: 640,
+	scale: 0.9,
+	startframe: 48,
+	endframe: 174,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: 476,
+	endLeft: 350,
+	scale: 0.8,
+	startframe: 53,
+	endframe: 122,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: 358,
+	endLeft: -30,
+	scale: 1,
+	startframe: 53,
+	endframe: 140,
+}, {
+	url: "balloon4.png",
+	balloon: spriteOption.balloon4,
+	top: h,
+	left: 306,
+	endLeft: -560,
+	scale: 1,
+	startframe: 59,
+	endframe: 120,
+}, {
+	url: "balloon4.png",
+	balloon: spriteOption.balloon4,
+	top: h,
+	left: 311,
+	endLeft: 176,
+	scale: 1.2,
+	startframe: 59,
+	endframe: 150,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: 427,
+	endLeft: 176,
+	scale: 1.2,
+	startframe: 79,
+	endframe: 166,
+}, {
+	url: "balloon1.png",
+	balloon: spriteOption.balloon1,
+	top: h,
+	left: -spriteOption.balloon1.width * 1 + 50,
+	endLeft: -spriteOption.balloon1.width * 1 + 100,
+	scale: 1,
+	startframe: 116,
+	endframe: 160,
+}, {
+	url: "balloon2.png",
+	balloon: spriteOption.balloon2,
+	top: h,
+	left: -40,
+	endLeft: 70,
+	scale: 1.2,
+	startframe: 88,
+	endframe: 150,
+}, {
+	url: "balloon3.png",
+	balloon: spriteOption.balloon3,
+	top: 381,
+	left: -spriteOption.balloon3.width,
+	endLeft: 66,
+	scale: 1,
+	startframe: 116,
+	endframe: 160,
+}]
+var imgageObjList = [];
+var balloonList = [];
+var planetSprite;
+var moveBottomToTop = function(progress, startTime) {
+	this.progress = progress;
+	this.startTime = startTime;
+	this.execute = function(sprite, context, currentTime) {
+		if (currentTime > this.startTime && this.progress >= 0) {
+			sprite.top -= sprite.velocityY;
+		}
+		this.progress--;
+	}
+}
+var balloonMoveBottomToTop = function(progress, startTime) {
+		this.progress = progress;
+		this.startTime = startTime;
+		this.execute = function(sprite, context, currentTime) {
+			if (currentTime > this.startTime && sprite.top > -sprite.height) {
+				sprite.visible = true;
+				sprite.top -= sprite.velocityY;
+				sprite.left += sprite.velocityX;
+			} else {
+				sprite.visible = false;
+			}
+		}
+	}
+	//云彩的运动
+var moveLeftToRight = function(progress, startTime) {
+	this.progress = progress;
+	this.startTime = startTime;
+	this.execute = function(sprite, context, currentTime) {
+
+		if (currentTime > this.startTime && this.progress >= 0) {
+			// 如果到了右边缘
+			if (sprite.left >= w) {
+				sprite.velocityX = -sprite.velocityX;
+				sprite.left = w;
+			}
+			// 检测是否到了左边缘
+			else if (sprite.left <= 0) {
+				sprite.velocityX = -sprite.velocityX;
+				sprite.left = 0;
+			}
+			// 底边缘
+			if (sprite.top >= h) {
+				sprite.velocityY = -sprite.velocityY;
+				sprite.top = h;
+			}
+
+			// 是否上边缘
+			else if (sprite.top <= h - planetSprite.height) {
+				sprite.velocityY = -sprite.velocityY;
+				sprite.top = h - planetSprite.height;
+			}
+			sprite.alpha = (h - sprite.top) / planetSprite.height;
+			sprite.left -= sprite.velocityX;
+			sprite.top -= sprite.velocityY;
+		}
+		sprite.progress--;
+	}
+}
+var flash = function(progress, startTime, flashCount) {
+	this.progress = progress;
+	this.duringTime = progress;
+	this.startTime = startTime;
+	this.index = 0;
+	this.execute = function(sprite, context, currentTime) {
+		if (currentTime > this.startTime && this.progress >= 0) {
+			sprite.visible = true;
+			sprite.alpha = Math.abs(curveFunction.sineEaseOut(currentTime - this.startTime, 0, 1, this.duringTime / flashCount));
+			this.progress--;
+		} else {
+			sprite.visible = false;
+		}
+	}
+}
+
+var cloudSpriteList = [];
+
+//星星的闪烁
+var starSpriteList = [];
+//气球
+var balloonSpriteList = [];
+
+loadImg(ImageUrlList, init, imgageObjList);
+
+function animation() {
+	if (totalProgress < TOTALTIME) {
+		totalProgress++;
+		ctx.globalCompositeOperation = "source-over";
+		ctx.clearRect(0, 0, w, h);
+		ctx.save();
+		planetSprite.update(ctx, totalProgress);
+		planetSprite.paint(ctx);
+
+		starSpriteList.forEach(function(item, index) {
+			item.update(ctx, totalProgress);
+			item.paint(ctx, totalProgress);
+		});
+		balloonSpriteList.forEach(function(item, index) {
+			item.update(ctx, totalProgress);
+			item.paint(ctx, totalProgress);
+		});
+		cloudSpriteList.forEach(function(item, index) {
+			item.update(ctx, totalProgress);
+			item.paint(ctx);
+		});
+		ctx.restore();
+		requestAnimFrame(animation);
+	} else {
+		console.log("动画结束");
+	}
+}
+
+function init() {
+	var planetframeStart = 0;
+	var planetframeEnd = 40;
+	var planetframeDuringTime = (planetframeEnd - planetframeStart) * FRAMESPEED;
+	planetSprite = new Sprite('星球', new ImagePainter(ImageUrlList.planet, spriteOption.planet.imgx, spriteOption.planet.imgy, spriteOption.planet.width, spriteOption.planet.height), [new moveBottomToTop(planetframeDuringTime, 0)]);
+	planetSprite.init = function() {
+		planetSprite.width = spriteOption.planet.width;
+		planetSprite.height = spriteOption.planet.height;
+		planetSprite.top = h;
+		planetSprite.left = 0;
+		//planetSprite.velocityY = 1;
+		planetSprite.velocityY = planetSprite.height / planetframeDuringTime;
+	}
+	planetSprite.init();
+	for (var i = 0; i < 10; i++) {
+		var cloudSprite = new Sprite('cloud' + i, new ImagePainter(ImageUrlList.cloud, spriteOption.cloud.imgx, spriteOption.cloud.imgy, spriteOption.cloud.width, spriteOption.cloud.height), [new moveLeftToRight(4540, 0)]);
+		cloudSprite.init = function() {
+			this.width = spriteOption.cloud.width * generateRandom(0.5, 1);
+			this.height = spriteOption.cloud.height * generateRandom(0.5, 1);
+			this.left = generateRandom(0, w);
+			this.velocityX = generateRandom(0.5, 1);
+			this.velocityY = generateRandom(0.5, 1);
+			this.top = h + this.height * generateRandom(0.5, 1);
+		}
+		cloudSpriteList.push(cloudSprite);
+	}
+	cloudSpriteList.forEach(function(item, index) {
+		item.init();
+	});
+
+	starSpriteOption.forEach(function(item, index) {
+		var startTime = FRAMESPEED * item.startframe;
+		var flashCount = item.flashCount || 6;
+		var duringTime = FRAMESPEED * (item.endframe - item.startframe);
+		var sprite = new Sprite('星星' + index, new ImagePainter(ImageUrlList.star, spriteOption.star.imgx, spriteOption.star.imgy, spriteOption.star.width, spriteOption.star.height), [new flash(duringTime, startTime, flashCount)]);
+		sprite.top = item.top;
+		sprite.left = item.left;
+		sprite.width = spriteOption.star.width * item.scale;
+		sprite.height = spriteOption.star.height * item.scale;
+		starSpriteList.push(sprite);
+	});
+	balloonSpriteOption.forEach(function(item, index) {
+		var startTime = FRAMESPEED * item.startframe;
+		var duringTime = FRAMESPEED * (item.endframe - item.startframe);
+		var sprite = new Sprite('气球' + index, new ImagePainter(item.url, item.balloon.imgx, item.balloon.imgy, item.balloon.width, item.balloon.height), [new balloonMoveBottomToTop(duringTime, startTime)]);
+		sprite.top = item.top;
+		sprite.left = item.left;
+		sprite.width = item.balloon.width * item.scale;
+		sprite.height = item.balloon.height * item.scale;
+		sprite.velocityY = (item.top + sprite.height) / duringTime;
+		sprite.velocityX = (item.endLeft - item.left) / duringTime;
+		balloonSpriteList.push(sprite);
+	})
+	animation();
+}
