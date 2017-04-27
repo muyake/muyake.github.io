a632fde53cff4646942b977eef1cb5141a34a14a
muyake <1245570818@qq.com>
2017-04-27 16:03:47 +0800

    速度变快

--- 2.js
+++ 2.js
@@ -3,8 +3,8 @@ var canvas = document.getElementById('mycanvas'),
 	w,
 	h,
 	maxCloudVelocity = 1,
-	FRAMESPEED = 10;
-var TOTALFRAME = 176;
+	FRAMESPEED = 4;
+var TOTALFRAME = 276;
 var TOTALTIME = FRAMESPEED * TOTALFRAME;
 var totalProgress = 0;
 var spriteOption = {
@@ -75,14 +75,6 @@ var spriteOption = {
 	totalcloud: {
 		width: 750,
 		height: 604,
-		imgx: 0,
-		imgy: 0,
-		x: 0,
-		y: 0
-	},
-	totalcloud2: {
-		width: 750,
-		height: 456,
 		imgx: 0,
 		imgy: 0,
 		x: 0,
@@ -167,168 +159,169 @@ var ImageUrlList = {
 	totalcloud: "./img/yanwu.png",
 };
 var balloonSpriteOption = [{
-	url: "balloon1.png",
-	balloon: spriteOption.balloon1,
-	top: h,
-	left: 220,
-	endLeft: 220,
-	scale: 1,
-	startframe: 0,
-	endframe: 72,
-}, {
-	url: "balloon5.png",
-	balloon: spriteOption.balloon5,
-	top: h,
-	left: -5,
-	endLeft: -25,
-	scale: 1,
-	startframe: 8,
-	endframe: 66,
-}, {
-	url: "balloon4.png",
-	balloon: spriteOption.balloon4,
-	top: h,
-	left: 326,
-	endLeft: 326,
-	scale: 1,
-	startframe: 13,
-	endframe: 178,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: 110,
-	endLeft: 110,
-	scale: 1,
-	startframe: 18,
-	endframe: 86,
-}, {
-	url: "balloon1.png",
-	balloon: spriteOption.balloon1,
-	top: h,
-	left: 308,
-	endLeft: 489,
-	scale: 1.3,
-	startframe: 27,
-	endframe: 90,
-}, {
-	url: "balloon3.png",
-	balloon: spriteOption.balloon3,
-	top: h,
-	left: 161,
-	endLeft: 278,
-	scale: 0.8,
-	startframe: 38,
-	endframe: 128,
-}, {
-	url: "balloon3.png",
-	balloon: spriteOption.balloon3,
-	top: h,
-	left: 220,
-	endLeft: 440,
-	scale: 1,
-	startframe: 38,
-	endframe: 104,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: -40,
-	endLeft: 114,
-	scale: 0.9,
-	startframe: 45,
-	endframe: 117,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: -40,
-	endLeft: 114,
-	scale: 0.9,
-	startframe: 45,
-	endframe: 117,
-}, {
-	url: "balloon4.png",
-	balloon: spriteOption.balloon4,
-	top: h,
-	left: 126,
-	endLeft: 640,
-	scale: 0.9,
-	startframe: 48,
-	endframe: 174,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: 476,
-	endLeft: 350,
-	scale: 0.8,
-	startframe: 53,
-	endframe: 122,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: 358,
-	endLeft: -30,
-	scale: 1,
-	startframe: 53,
-	endframe: 140,
-}, {
-	url: "balloon4.png",
-	balloon: spriteOption.balloon4,
-	top: h,
-	left: 306,
-	endLeft: -560,
-	scale: 1,
-	startframe: 59,
-	endframe: 120,
-}, {
-	url: "balloon4.png",
-	balloon: spriteOption.balloon4,
-	top: h,
-	left: 311,
-	endLeft: 176,
-	scale: 1.2,
-	startframe: 59,
-	endframe: 150,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: 427,
-	endLeft: 176,
-	scale: 1.2,
-	startframe: 79,
-	endframe: 166,
-}, {
-	url: "balloon1.png",
-	balloon: spriteOption.balloon1,
-	top: h,
-	left: -spriteOption.balloon1.width * 1 + 50,
-	endLeft: -spriteOption.balloon1.width * 1 + 100,
-	scale: 1,
-	startframe: 116,
-	endframe: 160,
-}, {
-	url: "balloon2.png",
-	balloon: spriteOption.balloon2,
-	top: h,
-	left: -40,
-	endLeft: 70,
-	scale: 1.2,
-	startframe: 88,
-	endframe: 150,
-}, {
-	url: "balloon3.png",
-	balloon: spriteOption.balloon3,
-	top: 381,
-	left: -spriteOption.balloon3.width,
-	endLeft: 66,
-	scale: 1,
-	startframe: 116,
-	endframe: 160,
-}]
+		url: "balloon1.png",
+		balloon: spriteOption.balloon1,
+		top: h,
+		left: 220,
+		endLeft: 220,
+		scale: 1,
+		startframe: 0,
+		endframe: 72,
+	}, {
+		url: "balloon5.png",
+		balloon: spriteOption.balloon5,
+		top: h,
+		left: -5,
+		endLeft: -25,
+		scale: 1,
+		startframe: 8,
+		endframe: 66,
+	}, {
+		url: "balloon4.png",
+		balloon: spriteOption.balloon4,
+		top: h,
+		left: 326,
+		endLeft: 326,
+		scale: 1,
+		startframe: 13,
+		endframe: 178,
+	}, {
+		url: "balloon1.png",
+		balloon: spriteOption.balloon1,
+		top: h,
+		left: 308,
+		endLeft: 489,
+		scale: 1.3,
+		startframe: 27,
+		endframe: 90,
+	}, {
+		url: "balloon3.png",
+		balloon: spriteOption.balloon3,
+		top: h,
+		left: 161,
+		endLeft: 278,
+		scale: 0.8,
+		startframe: 38,
+		endframe: 128,
+	},
+	//{
+	// 	url: "balloon3.png",
+	// 	balloon: spriteOption.balloon3,
+	// 	top: h,
+	// 	left: 220,
+	// 	endLeft: 440,
+	// 	scale: 1,
+	// 	startframe: 38,
+	// 	endframe: 104,
+	// },
+	// {
+	// 	url: "balloon2.png",
+	// 	balloon: spriteOption.balloon2,
+	// 	top: h,
+	// 	left: -40,
+	// 	endLeft: 114,
+	// 	scale: 0.9,
+	// 	startframe: 45,
+	// 	endframe: 117,
+	// },
+	{
+		url: "balloon2.png",
+		balloon: spriteOption.balloon2,
+		top: h,
+		left: -40,
+		endLeft: 114,
+		scale: 0.9,
+		startframe: 45,
+		endframe: 117,
+	}, {
+		url: "balloon4.png",
+		balloon: spriteOption.balloon4,
+		top: h,
+		left: 126,
+		endLeft: 640,
+		scale: 0.9,
+		startframe: 48,
+		endframe: 174,
+	},
+	//  {
+	// 	url: "balloon2.png",
+	// 	balloon: spriteOption.balloon2,
+	// 	top: h,
+	// 	left: 476,
+	// 	endLeft: 350,
+	// 	scale: 0.8,
+	// 	startframe: 53,
+	// 	endframe: 122,
+	// },
+	{
+		url: "balloon2.png",
+		balloon: spriteOption.balloon2,
+		top: h,
+		left: 358,
+		endLeft: -30,
+		scale: 1,
+		startframe: 53,
+		endframe: 140,
+	}, {
+		url: "balloon4.png",
+		balloon: spriteOption.balloon4,
+		top: h,
+		left: 306,
+		endLeft: -560,
+		scale: 1,
+		startframe: 59,
+		endframe: 120,
+	}, {
+		url: "balloon4.png",
+		balloon: spriteOption.balloon4,
+		top: h,
+		left: 311,
+		endLeft: 176,
+		scale: 1.2,
+		startframe: 59,
+		endframe: 150,
+	}, {
+		url: "balloon2.png",
+		balloon: spriteOption.balloon2,
+		top: h,
+		left: 427,
+		endLeft: 176,
+		scale: 1.2,
+		startframe: 79,
+		endframe: 166,
+	}
+	// , {
+	// 	url: "balloon1.png",
+	// 	balloon: spriteOption.balloon1,
+	// 	top: h,
+	// 	left: -spriteOption.balloon1.width * 1 + 50,
+	// 	endLeft: -spriteOption.balloon1.width * 1 + 100,
+	// 	scale: 1,
+	// 	startframe: 116,
+	// 	endframe: 160,
+	// }
+	// , {
+	// 	url: "balloon2.png",
+	// 	balloon: spriteOption.balloon2,
+	// 	top: h,
+	// 	left: -40,
+	// 	endLeft: 70,
+	// 	scale: 1.2,
+	// 	startframe: 88,
+	// 	endframe: 150,
+	// }
+	// , 
+	// {
+	// 	url: "balloon3.png",
+	// 	balloon: spriteOption.balloon3,
+	// 	top: 381,
+	// 	left: -spriteOption.balloon3.width,
+	// 	endLeft: 66,
+	// 	scale: 1,
+	// 	startframe: 116,
+	// 	endframe: 160,
+	// }
+]
 var imgageObjList = [];
 var balloonList = [];
 var planetSprite;
@@ -454,6 +447,7 @@ function animation() {
 		ctx.clearRect(0, 0, w, h);
 		ctx.save();
 
+
 		planetSprite.update(ctx, totalProgress);
 		planetSprite.paint(ctx);
 		starSpriteList.forEach(function(item, index) {
@@ -469,9 +463,13 @@ function animation() {
 		ctx.restore();
 		requestAnimFrame(animation);
 	} else {
+		entTime = Date.now();
+		console.log((entTime - startTime) / 1000);
 		console.log("动画结束");
 	}
 }
+var startTime = Date.now();
+var entTime;
 
 function init() {
 	var canvasParentWidth = canvas.parentNode.clientWidth;
@@ -481,7 +479,7 @@ function init() {
 	w = 750;
 	h = canvas.height;
 	var planetframeStart = 5;
-	var planetframeEnd = 30;
+	var planetframeEnd = 60;
 	var planetframeDuringTime = (planetframeEnd - planetframeStart) * FRAMESPEED;
 	planetSprite = new Sprite('星球', new ImagePainter(ImageUrlList.planet, spriteOption.planet.imgx, spriteOption.planet.imgy, spriteOption.planet.width, spriteOption.planet.height), [new moveBottomToTop(planetframeDuringTime, 0)]);
 	planetSprite.init = function() {
@@ -494,7 +492,7 @@ function init() {
 	planetSprite.init();
 
 
-	var cloudframeDuringTime = (20 - 0) * FRAMESPEED;
+	var cloudframeDuringTime = (40 - 0) * FRAMESPEED;
 	cloudSprite1 = new Sprite('yun1', new ImagePainter(ImageUrlList.totalcloud, spriteOption.totalcloud.imgx, spriteOption.totalcloud.imgy, spriteOption.totalcloud.width, spriteOption.totalcloud.height), [new moveBottomToTop(cloudframeDuringTime, 0), new totalLeftToRight(4540, 0)]);
 	cloudSprite1.init = function() {
 		cloudSprite1.width = spriteOption.totalcloud.width * 5.5;
@@ -524,9 +522,9 @@ function init() {
 	});
 
 	starSpriteOption.forEach(function(item, index) {
-		var startTime = FRAMESPEED * item.startframe;
+		var startTime = FRAMESPEED * (item.startframe + 50);
 		var flashCount = item.flashCount || 6;
-		var duringTime = FRAMESPEED * (item.endframe - item.startframe);
+		var duringTime = FRAMESPEED * ((item.endframe + 50) - item.startframe);
 		var sprite = new Sprite('星星' + index, new ImagePainter(ImageUrlList.star, spriteOption.star.imgx, spriteOption.star.imgy, spriteOption.star.width, spriteOption.star.height), [new flash(duringTime, startTime, flashCount)]);
 		sprite.top = item.top;
 		sprite.left = item.left;
@@ -535,8 +533,8 @@ function init() {
 		starSpriteList.push(sprite);
 	});
 	balloonSpriteOption.forEach(function(item, index) {
-		var startTime = FRAMESPEED * item.startframe;
-		var duringTime = FRAMESPEED * (item.endframe - item.startframe);
+		var startTime = FRAMESPEED * (item.startframe + 60);
+		var duringTime = FRAMESPEED * ((item.endframe + 60) - item.startframe);
 		var sprite = new Sprite('气球' + index, new ImagePainter(item.url, item.balloon.imgx, item.balloon.imgy, item.balloon.width, item.balloon.height), [new balloonMoveBottomToTop(duringTime, startTime)]);
 		item.top = h;
 		sprite.top = item.top;
