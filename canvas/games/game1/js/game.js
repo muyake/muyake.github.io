aec12ad3f14e12e6a090442a7d7694d93c0e420d
muyake <1245570818@qq.com>
2017-05-18 22:02:55 +0800

    代码整理，并实现进度条结束后显示游戏界面

--- canvas/games/game1/js/game.js
+++ canvas/games/game1/js/game.js
@@ -1,30 +1,34 @@
-var progressDiv = document.getElementById('load');
-//progressDiv.appendChild(progressbar.domElement);
-var progressbar = new COREHTML5.Progressbar(progressDiv, 'rgba(0,0,0,0.5)', 100, 130, 250);
-
-function progressCallback(num) {
-    progressbar.draw(num);
-}
-
-function progressOver() {
-    console.log('加载完成');
-}
-var l = new Loading(progressCallback, progressOver);
-var imageList = [
-    './image/grass/grass.png',
-    './image/runpeople.png',
-    './image/sky.png',
-    './image/tree/smalltree.png',
-    './image/tree/tree.png',
-    './image/tree/tree-twotrunks.png',
-    './image/imgBoom/bomb.png',
-    './image/imgBoom/bomb-no-fuse.png',
-];
-for (var i = 0; i < 9; i++) {
-    imageList.push("./image/imgBoom/explosion-0" + i + ".png");
-    imageList.push("./image/imgBoom/fuse-0" + i + ".png");
+var game = {
+    imageList: [
+        './image/grass/grass.png',
+        './image/runpeople.png',
+        './image/sky.png',
+        './image/tree/smalltree.png',
+        './image/tree/tree.png',
+        './image/tree/tree-twotrunks.png',
+        './image/imgBoom/bomb.png',
+        './image/imgBoom/bomb-no-fuse.png',
+    ],
+    mycanvas: document.getElementById('mycanvas'),
+    progressDiv: document.getElementById('load'),
+    progressCallback: function(num) {
+        this.progressbar.draw(num);
+    },
+    progressOver: function() {
+        this.mycanvas.style.display = 'block';
+        this.progressDiv.style.display = 'none';
+    },
+    init: function() {
+        this.progressbar = new COREHTML5.Progressbar(this.progressDiv, 'rgba(0,0,0,0.5)', 100, 130, 250);
+        var l = new Loading(this.progressCallback.bind(this), this.progressOver.bind(this));
+        for (var i = 0; i < 9; i++) {
+            this.imageList.push("./image/imgBoom/explosion-0" + i + ".png");
+            this.imageList.push("./image/imgBoom/fuse-0" + i + ".png");
+        };
+        this.imageList.forEach(function(item, index, arr) {
+            l.queueImage(item);
+        });
+        l.loadImages();
+    }
 };
-imageList.forEach(function(item, index, arr) {
-    l.queueImage(item);
-})
-l.loadImages();
+game.init();
---
+++ canvas/games/game1/js/libs/gameEngine.js
@@ -0,0 +1,55 @@
+var getTimeNow = function() {
+	return +new Date();
+};
+var Game = function(gameName, canvasId) {
+	var canvas = document.getElementById(canvasId),
+		self = this; // Used by key event handlers below
+
+	// General
+
+	this.context = canvas.getContext('2d');
+	this.gameName = gameName;
+	this.sprites = [];
+	this.keyListeners = [];
+
+	// Time
+
+	this.startTime = 0;
+	this.lastTime = 0;
+	this.gameTime = 0;
+	this.fps = 0;
+	this.STARTING_FPS = 60;
+
+	this.paused = false;
+	this.startedPauseAt = 0;
+	this.PAUSE_TIMEOUT = 100;
+
+}
+Game.prototype = {
+	start: function() {
+		var self = this;
+		this.startTime = getTimeNow();
+		window.requestNextAnimationFrame(function(time) {
+			self.animate.call(self, time);
+		});
+	},
+	animate: function(time) {
+		var self = this;
+		if (this.paused) {
+			//每隔100ms，看看是否还处于暂停状态，不需要太频繁。
+			setTimeout(function() {
+				window.requestNextAnimationFrame(function(time) {
+					self.animate.call(self, time);
+				});
+			}, this.PAUSE_TIMEOUT);
+		} else {
+			this.startAnimate(time);
+			window.requestNextAnimationFrame(function(time) {
+				self.animate.call(self, time);
+			});
+		}
+	},
+	startAnimate: function(time) {
+
+	},
+}
