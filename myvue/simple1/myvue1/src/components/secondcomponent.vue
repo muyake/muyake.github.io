ae9402c6f0ac59151b69a5f4ad3949d50166f35c
muyake <1245570818@qq.com>
2017-08-29 16:06:21 +0800

    添加mywebpack项目，计划将玛丽奥程序用es6写一遍

--- myvue/simple1/myvue1/src/components/secondcomponent.vue
+++ myvue/simple1/myvue1/src/components/secondcomponent.vue
@@ -3,20 +3,43 @@
     <h1>I am 第二页</h1>
     <a> written by {{ author }} </a>
     <p> 感谢 <a href="https://github.com/showonne">showonne</a>大神的技术指导</p>
+    <ul>
+      <li v-for="article in articles">
+        {{article.title}}
+      </li>
+    </ul>
   </div>
 </template>
- 
 <script>
 export default {
   data() {
     return {
       author: "微信公众号 jinkey-love",
       articles: [],
-    }
+
+      }
+    },
+    mounted: function() {
+        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+          headers: {
+
+          },
+          emulateJSON: true
+        }).then(function(response) {
+          // 这里是处理正确的回调
+          console.log();
+          this.articles = response.data.subjects
+          // this.articles = response.data["subjects"] 也可以
+
+        }, function(response) {
+          // 这里是处理错误的回调
+          console.log(response)
+        });
   }
-  }
+}
 
 </script>
- 
 <style>
+
+
 </style>
