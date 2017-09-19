3665c1433f416ef2de7b29b8fabc3f1c11651bcd
muyake <1245570818@qq.com>
2017-09-19 10:39:09 +0800

    图片修改

--- canvas/games/mario-es6/public/js/config.js
+++ canvas/games/mario-es6/public/js/config.js
@@ -2,7 +2,7 @@ let publicConfig = {
   GRAVITY_FORCE: 353 //9.8 * 2 * 18,
     //pixelsPerMeter : element.width / ARENA_LENGTH_IN_METERS,
 };
-let monsterConfig={
+let monsterConfig = {
 
   config: {
     totalCount: 5,
@@ -20,7 +20,7 @@ let monsterConfig={
       top: 0,
     },
 
-    
+
     sprite_2: {
       width: 50,
       height: 61,
@@ -28,7 +28,7 @@ let monsterConfig={
       top: 0,
     },
 
-  
+
 
     sprite_3: {
       width: 50,
@@ -39,14 +39,14 @@ let monsterConfig={
 
     sprite_4: {
       width: 50,
-      height: 61, 
+      height: 61,
       left: 240,
       top: 0,
     }
   }
 };
 
-let tortoiseConfig={
+let tortoiseConfig = {
 
   config: {
     totalCount: 12,
@@ -64,7 +64,7 @@ let tortoiseConfig={
       top: 0,
     },
 
-    
+
     sprite_9: {
       width: 54,
       height: 67,
@@ -72,7 +72,7 @@ let tortoiseConfig={
       top: 0,
     },
 
-  
+
 
     sprite_8: {
       width: 54,
@@ -83,11 +83,11 @@ let tortoiseConfig={
 
     sprite_7: {
       width: 54,
-      height: 67, 
+      height: 67,
       left: 216,
       top: 0,
     },
-     sprite_6: {
+    sprite_6: {
       width: 54,
       height: 67,
       left: 270,
@@ -101,13 +101,13 @@ let tortoiseConfig={
       top: 0,
     },
 
-    
+
     sprite_4: {
       width: 54,
       height: 67,
       left: 378,
       top: 0,
-    },  
+    },
 
     sprite_3: {
       width: 54,
@@ -118,11 +118,11 @@ let tortoiseConfig={
 
     sprite_2: {
       width: 54,
-      height: 67, 
+      height: 67,
       left: 486,
       top: 0,
     },
-     sprite_1: {
+    sprite_1: {
       width: 54,
       height: 67,
       left: 540,
@@ -138,7 +138,7 @@ let tortoiseConfig={
   }
 };
 
-let shellConfig={
+let shellConfig = {
 
   config: {
     totalCount: 12,
@@ -162,7 +162,7 @@ let shellConfig={
       top: 0,
     },
 
-    
+
     sprite_8: {
       width: 45,
       height: 43,
@@ -170,7 +170,7 @@ let shellConfig={
       top: 0,
     },
 
-  
+
 
     sprite_7: {
       width: 45,
@@ -181,11 +181,11 @@ let shellConfig={
 
     sprite_6: {
       width: 45,
-      height: 43, 
+      height: 43,
       left: 225,
       top: 0,
     },
-     sprite_5: {
+    sprite_5: {
       width: 45,
       height: 43,
       left: 405,
@@ -199,13 +199,13 @@ let shellConfig={
       top: 0,
     },
 
-    
+
     sprite_3: {
       width: 45,
       height: 43,
       left: 495,
       top: 0,
-    },  
+    },
 
     sprite_2: {
       width: 45,
@@ -216,73 +216,278 @@ let shellConfig={
 
     sprite_1: {
       width: 45,
-      height: 43, 
+      height: 43,
       left: 315,
       top: 0,
     },
-     sprite_0: {
+    sprite_0: {
       width: 45,
       height: 43,
       left: 360,
       top: 0,
     },
 
-  
+
   }
 };
-let lifeConfig={
-  config:{
-    totalCount:5,
-    sprite_1:{height:46,width:124,left: 0,top: 0,},
-    sprite_4:{height:47,width:127,left: 0,top: 46,},
-    sprite_3:{height:48,width:128,left: 0,top: 93,},
-    sprite_5:{height:48,width:129,left: 0,top: 141,},
-    sprite_2:{height:47,width:129,left: 0,top: 189,},
+let lifeConfig = {
+  config: {
+    totalCount: 5,
+    sprite_1: {
+      height: 46,
+      width: 124,
+      left: 0,
+      top: 0,
+    },
+    sprite_4: {
+      height: 47,
+      width: 127,
+      left: 0,
+      top: 46,
+    },
+    sprite_3: {
+      height: 48,
+      width: 128,
+      left: 0,
+      top: 93,
+    },
+    sprite_5: {
+      height: 48,
+      width: 129,
+      left: 0,
+      top: 141,
+    },
+    sprite_2: {
+      height: 47,
+      width: 129,
+      left: 0,
+      top: 189,
+    },
   },
 }
-let badFlowerConfig={
-  
-  config:{
-     totalCount: 35,
-    sprite_34:{height:6,width:40,left: 0,top: 0,},
-    sprite_8:{height:38,width:44,left: 0,top: 6,},
-    sprite_35:{height:5,width:45,left: 0,top: 44,},
-    sprite_28:{height:31,width:45,left: 0,top: 49,},
-    sprite_29:{height:27,width:45,left: 0,top: 80,},
-    sprite_19:{height:57,width:45,left: 0,top: 107,},
-    sprite_0:{height:11,width:45,left: 0,top: 164,},
-    sprite_33:{height:10,width:46,left: 0,top: 175,},
-    sprite_27:{height:32,width:46,left: 0,top: 185,},
-    sprite_9:{height:43,width:46,left: 0,top: 217,},
-    sprite_20:{height:53,width:46,left: 0,top: 260,},
-    sprite_7:{height:33,width:46,left: 0,top: 313,},
-    sprite_30:{height:22,width:47,left: 0,top: 346,},
-    sprite_2:{height:18,width:48,left: 0,top: 368,},
-    sprite_1:{height:15,width:48,left: 0,top: 386,},
-    sprite_26:{height:32,width:48,left: 0,top: 401,},
-    sprite_10:{height:49,width:48,left: 0,top: 433,},
-    sprite_23:{height:38,width:49,left: 0,top: 482,},
-    sprite_21:{height:51,width:49,left: 0,top: 520,},
-    sprite_3:{height:19,width:50,left: 0,top: 571,},
-    sprite_22:{height:48,width:51,left: 0,top: 590,},
-    sprite_11:{height:46,width:51,left: 0,top: 638,},
-    sprite_31:{height:19,width:51,left: 0,top: 684,},
-    sprite_32:{height:14,width:53,left: 0,top: 703,},
-    sprite_18:{height:58,width:53,left: 0,top: 717,},
-    sprite_12:{height:46,width:54,left: 0,top: 775,},
-    sprite_25:{height:31,width:54,left: 0,top: 821,},
-    sprite_13:{height:46,width:55,left: 0,top: 852,},
-    sprite_17:{height:58,width:55,left: 0,top: 898,},
-    sprite_24:{height:34,width:55,left: 0,top: 956,},
-    sprite_6:{height:30,width:56,left: 0,top: 990,},
-    sprite_4:{height:20,width:57,left: 0,top: 1020,},
-    sprite_14:{height:55,width:58,left: 0,top: 1040,},
-    sprite_5:{height:21,width:59,left: 0,top: 1095,},
-    sprite_15:{height:59,width:59,left: 0,top: 1116,},
-    sprite_16:{height:67,width:62,left: 0,top: 1175,},
+let badFlowerConfig = {
+
+    config: {
+      totalCount: 35,
+      sprite_34: {
+        height: 6,
+        width: 40,
+        left: 0,
+        top: 0,
+      },
+      sprite_8: {
+        height: 38,
+        width: 44,
+        left: 0,
+        top: 6,
+      },
+      sprite_35: {
+        height: 5,
+        width: 45,
+        left: 0,
+        top: 44,
+      },
+      sprite_28: {
+        height: 31,
+        width: 45,
+        left: 0,
+        top: 49,
+      },
+      sprite_29: {
+        height: 27,
+        width: 45,
+        left: 0,
+        top: 80,
+      },
+      sprite_19: {
+        height: 57,
+        width: 45,
+        left: 0,
+        top: 107,
+      },
+      sprite_0: {
+        height: 11,
+        width: 45,
+        left: 0,
+        top: 164,
+      },
+      sprite_33: {
+        height: 10,
+        width: 46,
+        left: 0,
+        top: 175,
+      },
+      sprite_27: {
+        height: 32,
+        width: 46,
+        left: 0,
+        top: 185,
+      },
+      sprite_9: {
+        height: 43,
+        width: 46,
+        left: 0,
+        top: 217,
+      },
+      sprite_20: {
+        height: 53,
+        width: 46,
+        left: 0,
+        top: 260,
+      },
+      sprite_7: {
+        height: 33,
+        width: 46,
+        left: 0,
+        top: 313,
+      },
+      sprite_30: {
+        height: 22,
+        width: 47,
+        left: 0,
+        top: 346,
+      },
+      sprite_2: {
+        height: 18,
+        width: 48,
+        left: 0,
+        top: 368,
+      },
+      sprite_1: {
+        height: 15,
+        width: 48,
+        left: 0,
+        top: 386,
+      },
+      sprite_26: {
+        height: 32,
+        width: 48,
+        left: 0,
+        top: 401,
+      },
+      sprite_10: {
+        height: 49,
+        width: 48,
+        left: 0,
+        top: 433,
+      },
+      sprite_23: {
+        height: 38,
+        width: 49,
+        left: 0,
+        top: 482,
+      },
+      sprite_21: {
+        height: 51,
+        width: 49,
+        left: 0,
+        top: 520,
+      },
+      sprite_3: {
+        height: 19,
+        width: 50,
+        left: 0,
+        top: 571,
+      },
+      sprite_22: {
+        height: 48,
+        width: 51,
+        left: 0,
+        top: 590,
+      },
+      sprite_11: {
+        height: 46,
+        width: 51,
+        left: 0,
+        top: 638,
+      },
+      sprite_31: {
+        height: 19,
+        width: 51,
+        left: 0,
+        top: 684,
+      },
+      sprite_32: {
+        height: 14,
+        width: 53,
+        left: 0,
+        top: 703,
+      },
+      sprite_18: {
+        height: 58,
+        width: 53,
+        left: 0,
+        top: 717,
+      },
+      sprite_12: {
+        height: 46,
+        width: 54,
+        left: 0,
+        top: 775,
+      },
+      sprite_25: {
+        height: 31,
+        width: 54,
+        left: 0,
+        top: 821,
+      },
+      sprite_13: {
+        height: 46,
+        width: 55,
+        left: 0,
+        top: 852,
+      },
+      sprite_17: {
+        height: 58,
+        width: 55,
+        left: 0,
+        top: 898,
+      },
+      sprite_24: {
+        height: 34,
+        width: 55,
+        left: 0,
+        top: 956,
+      },
+      sprite_6: {
+        height: 30,
+        width: 56,
+        left: 0,
+        top: 990,
+      },
+      sprite_4: {
+        height: 20,
+        width: 57,
+        left: 0,
+        top: 1020,
+      },
+      sprite_14: {
+        height: 55,
+        width: 58,
+        left: 0,
+        top: 1040,
+      },
+      sprite_5: {
+        height: 21,
+        width: 59,
+        left: 0,
+        top: 1095,
+      },
+      sprite_15: {
+        height: 59,
+        width: 59,
+        left: 0,
+        top: 1116,
+      },
+      sprite_16: {
+        height: 67,
+        width: 62,
+        left: 0,
+        top: 1175,
+      },
+    }
   }
-}
-//马里奥动画配置参数
+  //马里奥动画配置参数
 let marioConfig = {
 
   config: {
@@ -451,29 +656,29 @@ let WH = {
     width: 35,
     height: 35
   },
-  life:{
-    width:127*0.5,
-    height:48*0.5,
+  life: {
+    width: 127 * 0.5,
+    height: 48 * 0.5,
   },
-  badflower:{
-    width:55*0.5,
-    height:58*0.5,
+  badflower: {
+    width: 55 * 0.5,
+    height: 58 * 0.5,
   },
-  monster:{
-    width:50*0.4,
-    height:61*0.4
+  monster: {
+    width: 50 * 0.4,
+    height: 61 * 0.4
   },
-  tortoise:{
-    width:54*0.4,
-    height:67*0.4
+  tortoise: {
+    width: 54 * 0.4,
+    height: 67 * 0.4
   },
   tower: {
-    width: 279*0.85,
-    height: 342*0.6
+    width: 279 * 0.85,
+    height: 342 * 0.6
   },
-   shell: {
-    width: 45*0.4,
-    height: 43*0.4
+  shell: {
+    width: 45 * 0.4,
+    height: 43 * 0.4
   },
   hole: {
     width: 167 * 0.7,
@@ -517,12 +722,12 @@ let WH = {
     height: 94
   },
   flag: {
-    width: 81*0.5,
-    height: 70*0.5
+    width: 81 * 0.5,
+    height: 70 * 0.5
   },
-   final: {
-    width: 75*0.5,
-    height: 450*0.5
+  final: {
+    width: 75 * 0.5,
+    height: 450 * 0.5
   },
 }
 let wallConfig = {
@@ -577,19 +782,19 @@ let marioGameConfig = {
 }
 let element = {
   mycanvas: document.getElementById('mycanvas'),
-   mycanvasWidth: document.getElementById('mycanvas').width,
-    mycanvasHeight: document.getElementById('mycanvas').height,
+  mycanvasWidth: document.getElementById('mycanvas').width,
+  mycanvasHeight: document.getElementById('mycanvas').height,
   progressDiv: document.getElementById('load'),
   tipDiv: document.querySelector('.tip'),
   progressBox: document.querySelector('.loadDiv'),
 };
 let gameConfig = {
   // wallSpeed: 90,  // moneySpeed:90,
-  shellSpeed:100,
-  monsterSpeed:70,
+  shellSpeed: 100,
+  monsterSpeed: 70,
   objectSpeed: 60,
   progressObjSpeed: -2,
-  objectSpeedRate:60/(2),//表示显示的米数与实际的距离的比值。
+  objectSpeedRate: 60 / (2), //表示显示的米数与实际的距离的比值。
   skySpeed: 32,
   setSpeedDefault: function() {
     this.objectSpeed = 60;
@@ -605,4 +810,18 @@ let gameConfig = {
   roadHeight: 10,
 
 }
-export {publicConfig,monsterConfig,tortoiseConfig,shellConfig, lifeConfig,badFlowerConfig,marioConfig,bulletConfig,WH,wallConfig,marioGameConfig,element,gameConfig};
+export {
+  publicConfig,
+  monsterConfig,
+  tortoiseConfig,
+  shellConfig,
+  lifeConfig,
+  badFlowerConfig,
+  marioConfig,
+  bulletConfig,
+  WH,
+  wallConfig,
+  marioGameConfig,
+  element,
+  gameConfig
+};
