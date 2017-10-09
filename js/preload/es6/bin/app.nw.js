a9a6f2c1430a5db9c5bae9a9cff1b04d7be10c46
muyake <1245570818@qq.com>
2017-10-10 01:00:00 +0800

    es6版

--- js/preload/es6/bin/app.nw.js
+++ js/preload/es6/bin/app.nw.js
@@ -70,12 +70,91 @@
 "use strict";
 
 
-var a = 111;
-var b = 222;
-var xxx = function xxx(c, d) {
-  return c * d;
+var preloadImage = function preloadImage(path) {
+    return new Promise(function (resolve, reject) {
+        var image = new Image();
+        image.onload = resolve;
+        image.onerror = reject;
+        image.src = path;
+    });
 };
-console.log(xxx(a, b));
+var g = /*#__PURE__*/regeneratorRuntime.mark(function g() {
+    var foo;
+    return regeneratorRuntime.wrap(function g$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    _context.prev = 0;
+                    _context.next = 3;
+                    return getFoo();
+
+                case 3:
+                    foo = _context.sent;
+
+                    console.log(foo);
+                    _context.next = 10;
+                    break;
+
+                case 7:
+                    _context.prev = 7;
+                    _context.t0 = _context['catch'](0);
+
+                    console.log(_context.t0);
+
+                case 10:
+                case 'end':
+                    return _context.stop();
+            }
+        }
+    }, g, this, [[0, 7]]);
+});
+var preLoadObj = {
+    imgUrlArr: [],
+    //这里可以改正map对象http://www.cnblogs.com/sker/p/5520392.html
+    imgObjList: {},
+    progressCallback: function progressCallback() {},
+    progressOverCallback: function progressOverCallback() {},
+    setProgress: function setProgress() {
+        var length = this.imgUrlArr.length;
+        var count = 0;
+        var self = this;
+        var promiseArr = [];
+        var promises = this.imgUrlArr.map(function (item) {
+            return preloadImage(item);
+        });
+        Promise.all(promises).then(function (posts) {
+            self.progressOverCallback();
+        }, null, function (num) {
+            console.log(num);
+        }).catch(function (reason) {
+            // ...
+        });
+    },
+    init: function init(option) {
+        if (typeof option.imgUrlArr != 'undefined') {
+            this.imgUrlArr = option.imgUrlArr;
+        }
+        if (typeof option.progressCallback != 'undefined') {
+            this.progressCallback = option.progressCallback;
+        }
+        if (typeof option.progressOverCallback != 'undefined') {
+            this.progressOverCallback = option.progressOverCallback;
+        }
+        this.setProgress();
+    }
+};
+var option = {
+    imgUrlArr: ['../img/1.jpg', '../img/2.jpg', '../img/3.jpg', '../img/4.jpg', '../img/5.jpg', '../img/6.jpg'],
+    progressCallback: function progressCallback(progress) {
+        var barSelector = document.querySelector('.progress-bar');
+        barSelector.style.width = progress * 100 + '%';
+    },
+    progressOverCallback: function progressOverCallback() {
+        // console.log(preLoadObj.imgObjList['http://www.muyake.site/web/pubuliu/1.jpg'].src);
+        console.log('完成加载');
+    }
+};
+preLoadObj.init(option);
 
 /***/ })
 /******/ ]);
--- js/preload/es6/src/app.js
+++ js/preload/es6/src/app.js
@@ -1,4 +1,64 @@
-let a = 111;
-let b = 222;
-var xxx = (c, d) => c * d;
-console.log(xxx(a, b));
+const preloadImage = function(path) {
+    return new Promise(function(resolve, reject) {
+        var image = new Image();
+        image.onload = resolve;
+        image.onerror = reject;
+        image.src = path;
+    });
+};
+var g = function* () {
+  try {
+    var foo = yield getFoo();
+    console.log(foo);
+  } catch (e) {
+    console.log(e);
+  }
+};
+var preLoadObj = {
+    imgUrlArr: [],
+    //这里可以改正map对象http://www.cnblogs.com/sker/p/5520392.html
+    imgObjList: {},
+    progressCallback: function() {},
+    progressOverCallback: function() {},
+    setProgress: function() {
+        var length = this.imgUrlArr.length;
+        var count = 0;
+        var self = this;
+        var promiseArr = [];
+        var promises = this.imgUrlArr.map(function(item) {
+            return preloadImage(item);
+        });
+Promise.all(promises).then(function (posts) {
+  self.progressOverCallback();
+},null,function (num){
+    console.log(num);
+}).catch(function(reason){
+  // ...
+});
+
+    },
+    init: function(option) {
+        if (typeof option.imgUrlArr != 'undefined') {
+            this.imgUrlArr = option.imgUrlArr;
+        }
+        if (typeof option.progressCallback != 'undefined') {
+            this.progressCallback = option.progressCallback;
+        }
+        if (typeof option.progressOverCallback != 'undefined') {
+            this.progressOverCallback = option.progressOverCallback;
+        }
+        this.setProgress();
+    },
+}
+var option = {
+    imgUrlArr: ['../img/1.jpg', '../img/2.jpg', '../img/3.jpg', '../img/4.jpg', '../img/5.jpg', '../img/6.jpg'],
+    progressCallback: function(progress) {
+        var barSelector = document.querySelector('.progress-bar');
+        barSelector.style.width = (progress * 100) + '%';
+    },
+    progressOverCallback: function() {
+       // console.log(preLoadObj.imgObjList['http://www.muyake.site/web/pubuliu/1.jpg'].src);
+        console.log('完成加载');
+    },
+}
+preLoadObj.init(option);
