247ab243a948c65e19699ddb56b736bbc51c62ea
muyake <1245570818@qq.com>
2017-10-10 13:52:31 +0800

    es6 promise图片加载完成

--- js/preload/es6/bin/app.nw.js
+++ js/preload/es6/bin/app.nw.js
@@ -65,96 +65,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-
-var preloadImage = function preloadImage(path) {
-    return new Promise(function (resolve, reject) {
-        var image = new Image();
-        image.onload = resolve;
-        image.onerror = reject;
-        image.src = path;
-    });
-};
-var g = /*#__PURE__*/regeneratorRuntime.mark(function g() {
-    var foo;
-    return regeneratorRuntime.wrap(function g$(_context) {
-        while (1) {
-            switch (_context.prev = _context.next) {
-                case 0:
-                    _context.prev = 0;
-                    _context.next = 3;
-                    return getFoo();
-
-                case 3:
-                    foo = _context.sent;
-
-                    console.log(foo);
-                    _context.next = 10;
-                    break;
-
-                case 7:
-                    _context.prev = 7;
-                    _context.t0 = _context['catch'](0);
-
-                    console.log(_context.t0);
-
-                case 10:
-                case 'end':
-                    return _context.stop();
-            }
-        }
-    }, g, this, [[0, 7]]);
-});
-var preLoadObj = {
-    imgUrlArr: [],
-    //这里可以改正map对象http://www.cnblogs.com/sker/p/5520392.html
-    imgObjList: {},
-    progressCallback: function progressCallback() {},
-    progressOverCallback: function progressOverCallback() {},
-    setProgress: function setProgress() {
-        var length = this.imgUrlArr.length;
-        var count = 0;
-        var self = this;
-        var promiseArr = [];
-        var promises = this.imgUrlArr.map(function (item) {
-            return preloadImage(item);
-        });
-        Promise.all(promises).then(function (posts) {
-            self.progressOverCallback();
-        }, null, function (num) {
-            console.log(num);
-        }).catch(function (reason) {
-            // ...
-        });
-    },
-    init: function init(option) {
-        if (typeof option.imgUrlArr != 'undefined') {
-            this.imgUrlArr = option.imgUrlArr;
-        }
-        if (typeof option.progressCallback != 'undefined') {
-            this.progressCallback = option.progressCallback;
-        }
-        if (typeof option.progressOverCallback != 'undefined') {
-            this.progressOverCallback = option.progressOverCallback;
-        }
-        this.setProgress();
-    }
-};
-var option = {
-    imgUrlArr: ['../img/1.jpg', '../img/2.jpg', '../img/3.jpg', '../img/4.jpg', '../img/5.jpg', '../img/6.jpg'],
-    progressCallback: function progressCallback(progress) {
-        var barSelector = document.querySelector('.progress-bar');
-        barSelector.style.width = progress * 100 + '%';
-    },
-    progressOverCallback: function progressOverCallback() {
-        // console.log(preLoadObj.imgObjList['http://www.muyake.site/web/pubuliu/1.jpg'].src);
-        console.log('完成加载');
-    }
-};
-preLoadObj.init(option);
+throw new Error("Module build failed: SyntaxError: E:/mygit/back2/muyake.github.io/js/preload/es6/src/app.js: Unexpected character '；' (40:33)\n\n  38 |             self.progressOverCallback();\n  39 |         }).catch(function(reason) {\n> 40 |             console.log('图片加载错误')；\n     |                                  ^\n  41 |         });\n  42 |     },\n  43 |     init: function(option) {\n");
 
 /***/ })
 /******/ ]);
--- js/preload/es6/src/app.js
+++ js/preload/es6/src/app.js
@@ -1,43 +1,47 @@
-const preloadImage = function(path) {
-    return new Promise(function(resolve, reject) {
-        var image = new Image();
-        image.onload = resolve;
-        image.onerror = reject;
-        image.src = path;
-    });
-};
-var g = function* () {
-  try {
-    var foo = yield getFoo();
-    console.log(foo);
-  } catch (e) {
-    console.log(e);
-  }
-};
+var Promise = require('es6-promise').Promise;
+
 var preLoadObj = {
     imgUrlArr: [],
     //这里可以改正map对象http://www.cnblogs.com/sker/p/5520392.html
     imgObjList: {},
+    //图片加载进行中
     progressCallback: function() {},
+    //图片加载完成
     progressOverCallback: function() {},
+    preloadImage: function(src) {
+        var self = this;
+        return new Promise((resolve, reject) => {
+            var img = new Image();
+            self.imgObjList[src] = img;
+            img.onload = () => {
+                self.addProgress();
+                resolve();
+            };
+            img.onerror = () => {
+                // console.log(22);
+                reject();
+            };
+            img.src = src;
+        })
+    },
+    //进度条
+    addProgress: function() {},
     setProgress: function() {
         var length = this.imgUrlArr.length;
         var count = 0;
         var self = this;
         var promiseArr = [];
         var promises = this.imgUrlArr.map(function(item) {
-            return preloadImage(item);
+            return self.preloadImage(item);
         });
-Promise.all(promises).then(function (posts) {
-  self.progressOverCallback();
-},null,function (num){
-    console.log(num);
-}).catch(function(reason){
-  // ...
-});
-
+        Promise.all(promises).then(function(posts) {
+            self.progressOverCallback();
+        }).catch(function(reason) {
+            console.log('图片加载错误')；
+        });
     },
     init: function(option) {
+        var self = this;
         if (typeof option.imgUrlArr != 'undefined') {
             this.imgUrlArr = option.imgUrlArr;
         }
@@ -47,17 +51,25 @@ Promise.all(promises).then(function (posts) {
         if (typeof option.progressOverCallback != 'undefined') {
             this.progressOverCallback = option.progressOverCallback;
         }
+        this.addProgress = (function() {
+            var i = 0,
+                l = self.imgUrlArr.length;
+            return function() {
+                ++i;
+                self.progressCallback(i / l);
+            }
+        })();
         this.setProgress();
     },
 }
 var option = {
-    imgUrlArr: ['../img/1.jpg', '../img/2.jpg', '../img/3.jpg', '../img/4.jpg', '../img/5.jpg', '../img/6.jpg'],
+    imgUrlArr: ['http://www.muyake.site/web/pubuliu/1.jpg', 'http://www.muyake.site/web/pubuliu/2.jpg', 'http://www.muyake.site/web/pubuliu/3.jpg', 'http://www.muyake.site/web/pubuliu/4.jpg', 'http://www.muyake.site/web/pubuliu/5.jpg', 'http://www.muyake.site/web/pubuliu/6.jpg'],
     progressCallback: function(progress) {
         var barSelector = document.querySelector('.progress-bar');
         barSelector.style.width = (progress * 100) + '%';
     },
     progressOverCallback: function() {
-       // console.log(preLoadObj.imgObjList['http://www.muyake.site/web/pubuliu/1.jpg'].src);
+        console.log(preLoadObj.imgObjList['http://www.muyake.site/web/pubuliu/1.jpg'].src);
         console.log('完成加载');
     },
 }
